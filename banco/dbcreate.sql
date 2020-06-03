ac2d10f7402f39d4b1a1534cae293f0ea709fd6e
Ubuntu <ubuntu@ip-172-31-91-233.ec2.internal>
2020-06-03 22:17:37 +0000

    versao final atividade avaliativa

--- banco/dbcreate.sql
+++ banco/dbcreate.sql
@@ -1,14 +1,18 @@
-use database univille;
+USE `univille`;
 
 
 CREATE TABLE `academia` (
   `codigo` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(100) DEFAULT NULL,
   `horaini` varchar(200) DEFAULT NULL,
-  PRIMARY KEY (`codigo`)
-);
+  PRIMARY KEY (`codigo`));
 
+GRANT ALL PRIVILEGES ON univille.* TO 'root'@'%' WITH GRANT OPTION; 
+FLUSH PRIVILEGES;
 
+LOCK TABLES `academia` WRITE;
 insert into academia(nome, horaini) values ('Thaina', '10:00');
 insert into academia(nome, horaini) values ('Luan', '11:00');
 insert into academia(nome, horaini) values ('Matheus', '12:00');
+UNLOCK TABLES;
+
